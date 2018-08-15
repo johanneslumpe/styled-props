32b09704be174435101f9f68c9fb0d91c4fa24ff
Johannes Lumpe <johannes@johanneslumpe.de>
2018-08-14 22:56:07 -0400

    fix: make theme optional, return early if no theme or breakpoints present

--- src/__tests__/style.ts
+++ src/__tests__/style.ts
@@ -64,9 +64,7 @@ describe('style', () => {
   });
 
   it('should return `undefined` if specified prop is not set', () => {
-    const result = style<IProps>({ cssProp: 'output', prop: 'input' })({
-      theme: {},
-    });
+    const result = style<IProps>({ cssProp: 'output', prop: 'input' })({});
 
     expect(result).toBe(undefined);
   });
@@ -74,7 +72,6 @@ describe('style', () => {
   it('should assign the value of `prop` to `cssProp` and return an object if a value is set', () => {
     const result = style<IProps>({ cssProp: 'output', prop: 'input' })({
       input: 'value',
-      theme: {},
     });
 
     expect(result).toEqual({ output: 'value' });
@@ -129,8 +126,8 @@ describe('style', () => {
   it('should return the respective values for breakpoints', () => {
     const result = style<
       IColorProps<typeof theme.colors>,
-      ITheme,
-      IBreakpoints
+      typeof themeWithBreakpoints,
+      typeof themeWithBreakpoints.breakpoints
     >({
       cssProp: 'color',
       prop: 'input',
@@ -160,8 +157,8 @@ describe('style', () => {
   it('should return the value as is for each breakpoint, if no value can be found on the theme', () => {
     const result = style<
       IColorProps<{ [index: string]: string }>,
-      ITheme,
-      IBreakpoints
+      typeof themeWithBreakpoints,
+      typeof themeWithBreakpoints.breakpoints
     >({
       cssProp: 'color',
       prop: 'input',
@@ -191,8 +188,8 @@ describe('style', () => {
   it('should order media query results based on ordering within `theme.breakpoints` not based on ordering of the responsive object props', () => {
     const result = style<
       IColorProps<{ [index: string]: string }>,
-      ITheme,
-      IBreakpoints
+      typeof themeWithBreakpoints,
+      typeof themeWithBreakpoints.breakpoints
     >({
       cssProp: 'color',
       prop: 'input',
@@ -218,8 +215,8 @@ describe('style', () => {
   it('should allow `base` as value for breakpoints to define the base value (without media query) of a property', () => {
     const result = style<
       IColorProps<typeof theme.colors>,
-      ITheme,
-      IBreakpoints
+      typeof themeWithBreakpoints,
+      typeof themeWithBreakpoints.breakpoints
     >({
       cssProp: 'color',
       prop: 'input',
@@ -251,8 +248,8 @@ describe('style', () => {
   it('should put base values in front of all other values', () => {
     const result = style<
       IColorProps<typeof theme.colors>,
-      ITheme,
-      IBreakpoints
+      typeof themeWithBreakpoints,
+      typeof themeWithBreakpoints.breakpoints
     >({
       cssProp: 'color',
       prop: 'input',
@@ -273,5 +270,40 @@ describe('style', () => {
       themeWithBreakpoints.breakpoints.medium,
       themeWithBreakpoints.breakpoints.large,
     ]);
+  });
+
+  it('should return `undefined` if a responsive object is passed in but no theme and/or breakpoints are provided', () => {
+    const func = style<
+      IColorProps<typeof theme.colors>,
+      typeof themeWithBreakpoints,
+      typeof themeWithBreakpoints.breakpoints
+    >({
+      cssProp: 'color',
+      prop: 'input',
+      themeProp: 'colors',
+    });
+
+    const noThemeResult = func({
+      input: {
+        base: 'red',
+        large: 'red',
+        medium: 'green',
+        small: 'blue',
+      },
+      theme: null as typeof themeWithBreakpoints,
+    });
+
+    const noBreakpointsResult = func({
+      input: {
+        base: 'red',
+        large: 'red',
+        medium: 'green',
+        small: 'blue',
+      },
+      theme: {} as typeof themeWithBreakpoints,
+    });
+
+    expect(noThemeResult).toBe(undefined);
+    expect(noBreakpointsResult).toBe(undefined);
   });
 });
